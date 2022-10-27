cef8cafe026fa2f692f9aeaf6e222cfd5805d512
Vivian Li <vdl21@cam.ac.uk>
2022-10-28 00:24:41 +0100

    functions for loading audio in segments through librosa
    stubs for noise removal functions

--- src/tools/loader.py
+++ src/tools/loader.py
@@ -1,1 +1,64 @@
 # class for representing audio as a stream of segments
+
+import os
+
+import librosa
+
+from tools.noise import waveform_denoise, spectrogram_denoise
+
+class AudioSegment:
+    def __init__(self, data, sr, denoise = True):
+        self.data = data
+        self.sr = sr
+        self.denoise = denoise
+        # caching results
+        self._waveform = None
+        self._spectrogram = None
+    
+    def getWaveform(self):
+        if self._waveform is not None:
+            return self._waveform
+        waveform = self.data
+        if self.denoise:
+            waveform = waveform_denoise(waveform)
+        self._waveform = waveform
+        return waveform
+    
+    def getSpectrogram(self):
+        if self._spectrogram is not None:
+            return self._spectrogram
+        spectrogram = librosa.stft(self.data, n_fft=512, hop_length=256, window="hamming", center=False)
+        if self.denoise:
+            spectrogram = spectrogram_denoise(spectrogram)
+        self._spectrogram = spectrogram
+        return spectrogram
+    
+class AudioStream(object):
+    def __init__(self, file, sr = 22050, segment_length = 60):
+        self.file = file
+        self.sr = sr
+        self.segment_length = segment_length
+        # file metadata
+        self.duration = librosa.get_duration(file)
+        # iteration
+        self.position = 0
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        return self.next()
+        
+    def next(self):
+        if self.position < self.duration:
+            y = librosa.load(self.file, sr=self.sr, offset=self.position, duration=self.segment_length)
+            self.position += self.segment_length
+            return AudioSegment(y, sr=self.sr)
+        raise StopIteration()
+    
+def load_audio(file):
+    _, ext = os.path.splitext(file)
+    if ext != ".wav" or not os.path.exists(file):
+        return
+
+    return AudioStream(file)
--- src/tools/noise.py
+++ src/tools/noise.py
@@ -1,1 +1,7 @@
 # remove background noise of audio
+
+def waveform_denoise(data, frame_size):
+    pass
+
+def spectrogram_denoise(data):
+    pass
