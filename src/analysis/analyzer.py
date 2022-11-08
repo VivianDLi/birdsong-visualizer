682664ae26ebb77a0be7a0ccbbab84f4245624d2
Vivian Li <vdl21@cam.ac.uk>
2022-11-08 10:30:54 +0000

    class definitions for parallelizing acoustic index calculations with method stubs

--- src/analysis/analyzer.py
+++ src/analysis/analyzer.py
@@ -1,1 +1,38 @@
 # class for calculating acoustic indices given an audio segment
+
+from analysis.temporal_indices import *
+from analysis.spectral_indices import *
+from analysis.secondary_indices import *
+
+
+class Analyzer:
+    def __init__(self, segment: AudioSegment, indices):
+        index_mapping = {
+            "Ht": temporal_entropy,
+            "M": amplitude_median,
+            "BgN": background_noise,
+            "SNR": signal_to_noise_ratio,
+            "AcAct": acoustic_activity,
+            "AEFrac": acoustic_event_proportion_and_duration,
+            "AEDur": acoustic_event_proportion_and_duration,
+            "Hf": spectral_entropy,
+            "HfVar": spectral_entropy,
+            "HfMax": spectral_entropy,
+            "SpDiv": spectral_diversity,
+            "SpAct": spectral_activity,
+            "ACI": acoustic_complexity_index,
+            "AEI": acoustic_evenness_index,
+            "BioI": bioacoustic_index,
+            "LFreqCov": frequency_band_cover,
+            "MFreqCov": frequency_band_cover,
+            "HFreqCov": frequency_band_cover,
+            "NDSI": normalized_difference_soundscape_index,
+            "ARI": acoustic_richness_index,
+            "H": acoustic_entropy
+        }
+        self.index_functions = set([index_mapping[index] for index in indices])
+        self.indices = indices
+        self.segment = segment
+
+    def calculateIndices(self):
+        pass
--- src/analysis/coordinator.py
+++ src/analysis/coordinator.py
@@ -1,1 +1,17 @@
 # coordinate multiple analyzers in parallel
+
+class AnalysisCoordinator:
+    def __init__(self, stream: AudioStream, indices):
+        supported_indices = ["Ht", "M", "BgN", "SNR", "AcAct", "AEFrac", "AEDur", "Hf", "HfVar", "HfMax",
+                             "SpDiv", "SpAct", "ACI", "AEI", "BioI", "LFreqCov", "MFreqCov", "HFreqCov", "NDSI", "ARI", "H"]
+        if len(indices) > 3:
+            raise ValueError(
+                "Only up to three acoustic indices should be specified.")
+        if any([i in supported_indices for i in indices]):
+            raise ValueError("Unsupported acoustic indices were specified.")
+        self.stream = stream
+        self.indices = indices
+        self.analyzers = [Analyzer(segment, indices) for segment in stream]
+
+    def calculateIndices(self):
+        pass
